5220fd39aafbd5c4639e7505716febaf53370bcf
shahalam68 <a.k.mshahalam68@gmail.com>
2026-02-02 13:16:49 +0600

    error

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,29 +1,3 @@
-import type { ReactNode } from "react";
-import { NavLink } from "react-router-dom";
-import { protectedNavItems } from "../../constants";
-import { Collapse } from "antd";
-
-type Props = {
-  to: string;
-  children: ReactNode;
-};
-
-const SidebarNavLink = ({ to, children }: Props) => {
-  const className =
-    "text-white hover:text-primary rounded-lg font-medium px-4 py-1.5";
-  const activeClassName =
-    "bg-primary text-white rounded-lg font-medium px-4 py-1.5";
-
-  return (
-    <NavLink
-      to={to}
-      className={({ isActive }) => (isActive ? activeClassName : className)}
-    >
-      {children}
-    </NavLink>
-  );
-};
-
 const Sidebar = () => {
   return (
     <div className="md:col-span-2 h-max ">
