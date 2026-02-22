2e6bad4924c8817f949a0d107c3b916a2c0f7825
shahalam68 <a.k.mshahalam68@gmail.com>
2026-02-22 15:13:51 +0600

    slider in crouse catagory

--- src/pages/main/Home/components/CategorySection/CategorySection.tsx
+++ src/pages/main/Home/components/CategorySection/CategorySection.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import Container from "../../../../../components/common/Container/Container";
 import TitleCardOne from "../../../../../components/common/TitleCardOne/TitleCardOne";
 import { courseCategoryService } from "../../../../../store/services/courseCategoryService";
@@ -23,25 +26,41 @@ const CategorySection = () => {
             <Container>
                 <TitleCardOne text="Our Courses" />
 
-                <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-3 lg:gap-6 mb-6">
-                    {categories.map((category) => (
-                        <Link
-                            to={`/courses?category=${category._id}`}
-                            key={category._id}
-                        >
-                            <div
-                                key={category._id}
-                                className="flex flex-col justify-center items-center gap-2 bg-gradient-to-r from-green-500/20 via-emerald-500/70 to-green-600/70 text-white px-3 py-4 rounded-lg cursor-pointer text-sm lg:text-xl hover:scale-[1.1] transition-all"
-                            >
-                                <img
-                                    src={category.image}
-                                    alt=""
-                                    className="w-10 lg:w-14 mb-2"
-                                />
-                                <span>{category.name}</span>
-                            </div>
-                        </Link>
-                    ))}
+                <div className="mb-6 mt-4">
+                    <Swiper
+                        modules={[Autoplay]}
+                        spaceBetween={20}
+                        slidesPerView={2}
+                        breakpoints={{
+                            768: { slidesPerView: 3 },
+                            1024: { slidesPerView: 5 },
+                        }}
+                        autoplay={{
+                            delay: 2000,
+                            disableOnInteraction: false,
+                        }}
+                        loop={true}
+                        className="mySwiper relative"
+                    >
+                        {categories.map((category) => (
+                            <SwiperSlide key={category._id}>
+                                <Link
+                                    to={`/courses?category=${category._id}`}
+                                >
+                                    <div
+                                        className="flex flex-col justify-center items-center gap-2 bg-gradient-to-r from-green-500/20 via-emerald-500/70 to-green-600/70 text-white px-3 py-4 rounded-lg cursor-pointer text-sm lg:text-xl hover:scale-[1.05] transition-all"
+                                    >
+                                        <img
+                                            src={category.image}
+                                            alt=""
+                                            className="w-10 lg:w-14 mb-2"
+                                        />
+                                        <span className="text-center">{category.name}</span>
+                                    </div>
+                                </Link>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
                 </div>
             </Container>
         </div>
